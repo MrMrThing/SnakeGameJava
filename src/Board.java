7d8331e330a74ecc7eab099eaa9db35defad9b13
MrMrThing <rasmus031299@gmail.com>
2021-11-04 16:45:23 +0100

    Starting files

---
+++ src/Board.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Board {
+    
+    public Board(JFrame frame){
+        
+    }
+
+}
---
+++ src/main.java
@@ -0,0 +1,19 @@
+import javax.swing.JFrame;
+import java.awt.*;
+
+public class main{
+
+    public static void main(String[] args){
+
+        JFrame frame = new JFrame();
+        
+        frame.setSize(1000,1000);
+        frame.getContentPane().add(new Board(frame));
+        frame.setBackground(Color.LIGHT_GRAY);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+        Board Board = new Board(frame);
+    }
+
+}
