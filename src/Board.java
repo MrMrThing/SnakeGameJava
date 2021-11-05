5cc0d322dbb70a34773a06071aa9d20faf89ed70
MrMrThing <rasmus031299@gmail.com>
2021-11-05 08:51:35 +0100

    Update Board.java

--- src/Board.java
+++ src/Board.java
@@ -7,21 +7,25 @@ import java.awt.event.KeyListener;
 import java.util.ArrayList;
 
 
+
 public class Board extends JPanel implements KeyListener{
     
     int dx = 300;
     int dy = 300;
     JFrame frame;
     int size = 25;
+    int tempx = 300;
+    int tempy = 300;
+
+    char[] tail = new char[5];
+
     
-    public ArrayList<Integer> tail = new ArrayList<>();
-
-    this.tail.add(2,5);
-
     public Board(JFrame frame){
 
         this.frame = frame;
         frame.addKeyListener(this);
+
+        
     }
 
  
@@ -29,13 +33,35 @@ public class Board extends JPanel implements KeyListener{
     
     public void paint(Graphics g){
 
-        tail[2] = [2][5];
 
         g.fillRect(dx, dy, size, size);
 
         for(int i = 0; i < tail.length; i++){
             
-        }
+            switch(tail[i-1]){
+
+                case 'R':
+                  tempx = tempx - 50;
+                  g.fillRect(tempx, tempy, size, size);
+                  tail[i] = 'R';
+                  break;
+                case 'L':
+                  tempx = tempx + 50;
+                  g.fillRect(tempx, tempy, size, size);
+                  tail[i] = 'L';
+                  break;
+                case 'U':
+                  tempy = tempy + 50;
+                  g.fillRect(tempx, tempy, size, size);
+                  tail[i] = 'U';
+                  break;
+                case 'D':
+                  tempy = tempy - 50;
+                  g.fillRect(tempx, tempy-50, size, size);
+                  tail[i] = 'D';
+                  break;
+            }
+        }       
     }
 
     @Override
